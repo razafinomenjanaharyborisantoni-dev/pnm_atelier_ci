7597ccd9f7ef362cd7e646e21f211a67f40827d5
Romuald <romuald.andriatinarivo@student.passerellesnumeriques.org>
2026-05-15 16:52:30 +0300

    Romualdo

--- wallet.py
+++ wallet.py
@@ -3,6 +3,12 @@ class Wallet:
         self.balance = balance
 
     def set_balance(self, val):
+        """Adds a value to balance
+        Args:
+            val (float): Value to be added to balance.
+        Returns:
+            float: The addition of balance and val
+        """  
         self.balance = self.balance + val
 
     def get_balance(self):
